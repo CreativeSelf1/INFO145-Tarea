83ed52d53a93b58e6827d2558a049a38765d1760
CreativeSelf1 <fernacastillo.perez0607@gmail.com>
2024-06-10 17:42:15 -0400

    funciones

--- 2.1/arreglo_explicito.cpp
+++ 2.1/arreglo_explicito.cpp
@@ -1,23 +1,46 @@
 #include <iostream>
+#include <random>
+#include <iostream>
+#include <algorithm>
  
 using namespace std;
 
-int* generadorLineal(int n){
+void imprimirArreglo(int* arreglo, int n){
+    for (int i = 0; i < n; i++){
+        cout << arreglo[i] << " ";
+    }
+    
+}
+void Busqueda_binaria(int* lineal, int tamano, int search){
+    bool encontrado = binary_search(lineal, lineal + tamano, search);
+
+    cout << encontrado << endl;
+}
+
+int* GeneradorLineal(int n) {
     int* lineal = new int[n];
+
+    lineal[0] = rand() % 100;    // desde que rango?
+
+    for (int i = 1; i < n; i++) {
+        lineal[i] = lineal[i-1] + rand() % 10;   //epsilon que valor toma?
+    }
     
     return lineal;
 }
 
-int* generadorNormal(int n){
+int* GeneradorNormal(int n){
     int* normal = new int[n];
-    
     return normal;
 }
 
 int main(int argc, char *argv[]) {
-    int n = 10000;
+    int n = 10;
 
-    int* lineal = generadorLineal(n);
-    int* normal = generadorNormal(n);
+    int* lineal = GeneradorLineal(n);
+    int* normal = GeneradorNormal(n);
+
+    //imprimirArreglo(lineal, n);
+    Busqueda_binaria(lineal, n, 104);
 
 }

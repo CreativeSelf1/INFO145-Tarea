05fef356cce40d8b551f3c5efd149dc4dea899c4
Ingo <ingooxs@gmail.com>
2024-06-28 14:24:38 -0400

    Correccion busqueda caso 2.2
    Se corrigio la busqueda que se estaba realizando en el arreglo principal, ahora se busca en el arreglo gap-coded.

--- BinarySearch/binarySearch.cpp
+++ BinarySearch/binarySearch.cpp
@@ -24,25 +24,35 @@ bool gapCodingSearch(vector<int>& arr, vector<int>& gapCodingArray, vector<int>&
     int m = sampleArray.size();
     int left = 0;
     int right = 0;
+    int ct, cta = 0;
+    int mid;
     for (int i = 0; i<m; i++){
         if (sampleArray[i]<key){
             left = i;
+            ct = sampleArray[i];
         }
         else{
-            right= i;
+            right = i;
             break;
         }
     }
-    left = left * b;
     if (right == 0){
-        right= sampleArray.size()-1;
+        right = gapCodingArray.size()-1;
     }
-
+    else{
+        right = right * b;
+    }
+    left = left * b;
+    cta = ct;
+    
     while (left <= right) {
-        int mid = (left + right) / 2;
-        if (arr[mid] == key) {
+        mid = (left + right) / 2;
+        for (int i = left; i< mid; i++){
+            cta += gapCodingArray[i];
+        }
+        if (cta == key) {
             return true;
-        } else if (arr[mid] < key) {
+        } else if (cta < key) {
             left = mid + 1;
         } else {
             right = mid - 1;
